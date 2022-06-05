f80060f0a9cf01ff9a3706fb0df8e5422f92cb93
Marcelo Kevin <marcelo.kevinisj@gmail.com>
2022-06-05 04:14:41 -0300

    feat: creat user model

---
+++ src/model/user.js
@@ -0,0 +1,12 @@
+const connection = require("./connection");
+
+const getUser = async() =>{
+  const sql = "SELECT * FROM SearchUser.users";
+  
+  const [users] = await connection.execute(sql);
+  //console.log(users);
+  return users;
+}  
+
+
+module.exports = getUser;
